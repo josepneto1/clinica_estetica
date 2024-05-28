1df88a8f7152753ae6157d5c048d61a03e89c910
jpneto <passagnolo1@gmail.com>
2024-05-28 14:45:33 -0300

    Projeto para clínica de estética

---
+++ src/main/java/org/example/clinica_estetica/ClinicaEsteticaApplication.java
@@ -0,0 +1,13 @@
+package org.example.clinica_estetica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaEsteticaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClinicaEsteticaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/clinica_estetica/ClinicaEsteticaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.clinica_estetica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaEsteticaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
